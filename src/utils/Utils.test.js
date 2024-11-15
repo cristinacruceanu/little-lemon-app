e66cfb3889afd2c526f1c99589d757c7fbebb9a5
Cristina Cruceanu <cristinacruceanu@outlook.com>
2024-11-15 14:47:13 +0200

    Testing for initializeTimes updated

--- src/utils/Utils.test.js
+++ src/utils/Utils.test.js
@@ -1,25 +1,35 @@
 import { initializeTimes } from "./Utils";
+import { fetchAPI } from "../api";
 
-test("initializeTimes should return the correct expected values", () => {
-  const result = initializeTimes();
-  expect(result).toEqual({
-    availableTimes: [
-      "12:00 PM",
-      "1:00 PM",
-      "2:00 PM",
-      "3:00 PM",
-      "4:00 PM",
-      "5:00 PM",
-      "6:00 PM",
-      "7:00 PM",
-      "8:00 PM",
-    ],
-    formData: {
-      date: "",
-      time: "",
-      diners: "",
-      occasion: "",
-      seating: "",
-    },
+jest.mock("../api", () => ({
+  fetchAPI: jest.fn(),
+}));
+
+/*jest.clearAllMocks() - Ensure that any side-effects like mock functions or timers are cleaned up, to avoid interference.*/
+
+describe("initializeTimes", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("initializeTimes should return the correct expected values", async () => {
+    const mockTimes = ["12:00 PM", "1:00 PM"];
+    fetchAPI.mockResolvedValue(mockTimes);
+
+    const result = await initializeTimes();
+    expect(result).toEqual({
+      availableTimes: mockTimes,
+      formData: {
+        date: new Date().toISOString().split("T")[0],
+        time: "",
+        diners: "",
+        occasion: "",
+        seating: "",
+      },
+    });
+
+    expect(fetchAPI).toHaveBeenCalledWith(
+      new Date().toISOString().split("T")[0],
+    );
   });
 });
